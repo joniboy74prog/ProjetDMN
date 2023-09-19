030335bbdbce1bf9f5c4f49b1c9d74ef5ab09260
JKwK1308 <jankowiak@gmx.fr>
2023-09-19 12:29:22 +0200

    test

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,10 +1,44 @@
 package org.example;
 
+import java.util.Arrays;
+
+import org.kie.api.KieServices;
+import org.kie.api.builder.ReleaseId;
+import org.kie.api.runtime.KieContainer;
+import org.kie.dmn.api.core.DMNContext;
+import org.kie.dmn.api.core.DMNDecisionResult;
+import org.kie.dmn.api.core.DMNModel;
+import org.kie.dmn.api.core.DMNResult;
+import org.kie.dmn.api.core.DMNRuntime;
+import lombok.extern.slf4j.Slf4j;
+
+
 
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        KieServices kieServices = KieServices.Factory.get();
+        KieContainer kieContainer = kieServices.getKieClasspathContainer();
+
+
+        DMNRuntime dmnRuntime = kieContainer.newKieSession().getKieRuntime(DMNRuntime.class);
+        String namespace = "http://www.redhat.com/_c7328033-c355-43cd-b616-0aceef80e52a";
+        String modelName = "dmn-movieticket-ageclassification";
+        DMNModel dmnModel = dmnRuntime.getModel(namespace, modelName);
+
+        DMNContext dmnContext = dmnRuntime.newContext();  
+
+        for (Integer age : Arrays.asList(1,12,13,64,65,66)) {
+            dmnContext.set("Age", age);  
+            DMNResult dmnResult =
+                dmnRuntime.evaluateAll(dmnModel, dmnContext);  
+
+            for (DMNDecisionResult dr : dmnResult.getDecisionResults()) {  
+                log.info("Age: " + age + ", " +
+                        "Decision: '" + dr.getDecisionName() + "', " +
+                        "Result: " + dr.getResult());
+        }
+        }
     }
 }
