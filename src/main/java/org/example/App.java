2a4f1493971d33301d8829cada305ab9256baeeb
joniboy74prog <joniboymataxibos74@gmail.com>
2023-12-20 15:01:00 +0100

    Passage de la solution trouvée pour recupération automatique des noms des variables dans une branche locale du projet. TODO: réimplémenter le serveur ws :)))

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,32 +1,4 @@
 package org.example;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.InetSocketAddress;
-
 public class App {
-
-    public static void main(String[] args)  throws InterruptedException, IOException {
-        {
-            int port = 8025; // 843 flash policy port
-            Serveur s = new Serveur(new InetSocketAddress("localhost", port));
-            s.setReuseAddr(true);
-            s.start();
-            System.out.println("Serveur démarre sur le port:  " + s.getPort()
-                    + "\n Tapez \"exit\" pour sortir");
-            BufferedReader sysin = new BufferedReader(new InputStreamReader(System.in));
-            while (true) {
-                String in = sysin.readLine();
-                s.broadcast(in);
-                if (in.equals("exit")) {
-                    s.stop(1000);
-                    break;
-                }
-            }
-
-
-        }
-    }
 }
-
